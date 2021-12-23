c544d0412c0e3ba57e731afaef9a9691988bab13
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 18:55:07 +0100

    fix: testing and fixing

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import cors from 'cors'
+// const cool = require('cool-ascii-faces');
+// const express = require('express');
+// const path = require('path');
+// const PORT = process.env.PORT || 5000;
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 import router from './router'
 
-const cool = require('cool-ascii-faces');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
@@ -10,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app
+  // .set('view engine', 'ejs')
   .use(cors())
   .use(express.json())
   .use(router)
--- router.js
+++ router.js
@@ -1,3 +1,15 @@
 import express from 'express';
 
 const router = express.Router();
+
+router.route('/')
+  .get((_, res) => {
+    res.json(require('./config/db.json'));
+  })
+
+// router.get("", );
+// router.post("", );
+// router.put("", );
+// router.delete("", );
+
+export default router;
