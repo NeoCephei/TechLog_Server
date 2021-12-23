85c4edfd34064d74300797da789ff7b6678a89d2
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 19:18:31 +0100

    fix: add config var heroku dashboard

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_URL;
-console.log('>>>>>',uri);
+console.log('>>>>',uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
