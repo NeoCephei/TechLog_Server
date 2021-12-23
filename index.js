664949bb690596982da8d70246ff117000b5bb66
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 19:59:24 +0100

    fix: fixing in general

--- index.js
+++ index.js
@@ -1,64 +1,22 @@
-// const express = require('express');
-// const cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // const router = require('./router')
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// const app = express();
-
-// app
-//   // .set('view engine', 'ejs')
-//   .use(cors())
-//   .use(express.json())
-//   .use(router)
-
-// (async function () {
-//   try {
-//     app.listen(PORT, () => console.log('Server running on port ' + PORT));
-//   } catch (err) {
-//     console.error(err);
-//   }
-// })()
-
-// index.js
-
-const express = require("express");
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
 
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
+app
+  // .set('view engine', 'ejs')
+  .use(cors())
+  .use(express.json())
+  // .use(router)
 
-const port = 3031;
-
-const postsRouter = require("./routes");
-
-app.use(logger("dev"));
-
-const dbUrl = process.env.MONGO_URL;
-
-var options = {
-  keepAlive: 1,
-  connectTimeoutMS: 30000,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-mongoose.connect(dbUrl, options, (err) => {
-  if (err) console.log(err);
-});
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use("/posts", postsRouter);
-
-app.listen(port, function () {
-  console.log("Runnning on " + port);
-});
-module.exports = app;
+(async function () {
+  try {
+    app.listen(PORT, () => console.log('Server running on port ' + PORT));
+  } catch (err) {
+    console.error(err);
+  }
+})()
--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
-// // const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGO_URL;
 
-// const mongoose = require('mongoose');
-// const uri = process.env.MONGO_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// module.exports = mongoose;
+module.exports = client;
