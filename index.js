4eec7225ef5fb1361d79bf8225ad43381890bf46
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 20:37:03 +0100

    fix: fixing in general

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
 
-// const router = require('./router')
+// // const router = require('./router')
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-const app = express();
+// const app = express();
 
-app
-  // .set('view engine', 'ejs')
-  .use(cors())
-  .use(express.json())
-  // .use(router)
+// app
+//   // .set('view engine', 'ejs')
+//   .use(cors())
+//   .use(express.json())
+//   // .use(router)
 
-(async function () {
-  try {
-    app.listen(PORT, () => console.log('Server running on port ' + PORT));
-  } catch (err) {
-    console.error(err);
-  }
-})()
+// (async function () {
+//   try {
+//     app.listen(PORT, () => console.log('Server running on port ' + PORT));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// })()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`)
+})
