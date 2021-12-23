8b67896f64c7459163697bacf73d9eef6cc0d85b
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 18:50:09 +0100

    feat: server setup with old controllers + models

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+import cors from 'cors'
+import router from './router'
+
 const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
+
 const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const app = express();
+
+app
+  .use(cors())
+  .use(express.json())
+  .use(router)
+
+(async function () {
+  try {
+    app.listen(PORT, () => console.log('Server running on port ' + PORT));
+  } catch (err) {
+    console.error(err);
+  }
+})()
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGO_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = client;
