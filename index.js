1493beebde095751da90c2bcb66084c2ebd0e485
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 20:39:45 +0100

    fix: fixing in general

--- index.js
+++ index.js
@@ -23,12 +23,11 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at ${port}`)
+  console.log(`Server Running finally!!`)
 })
