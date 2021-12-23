c9d541775e0f94d6168fdb70ff1187b3dc5d5603
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 20:44:41 +0100

    fix: fixing in general

--- index.js
+++ index.js
@@ -21,13 +21,19 @@
 //   }
 // })()
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res
+    .status(200)
+    .send('Hello server is running')
+    .end();
+});
 
-app.listen(port, () => {
-  console.log(`Server Running finally!!`)
-})
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
