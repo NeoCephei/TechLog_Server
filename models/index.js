ca31fc68beb0928c7abb8728aa33dd24527545af
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-23 19:12:54 +0100

    fix: mongo url

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_URL;
+console.log('>>>>>',uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = client;
