cdc4cb56a9a88cb7a4e0a6f5c523d013b82d3f12
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-26 14:53:25 +0100

    fix: we keep fixing things

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const questions = require("./controllers/questions.controllers");
+const questions = require("./controllers/question.controllers");
 
 router.get("/", questions.sendMessage);
 
