7362302a0a180137690536d8d55e3b3631acdb2a
NeoCephei <77801754+NeoCephei@users.noreply.github.com>
2021-12-26 15:36:34 +0100

    fix: we keep fixing things

--- router.js
+++ router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const questionController = require("./controllers/question.controllers");
 
-router.get("/", questionController.greeting);
-router.get("/questions", questionController.getQuestions);
-router.post("/questions", questionController.postQuestion);
+router.get("/", questionController.greeting());
+router.get("/questions", questionController.getQuestion());
+router.post("/questions", questionController.postQuestion());
 
 
 module.exports = router;
